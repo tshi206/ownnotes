458110b33c4cf6ffe92349c8724ac77b098f0fc4
tshi206 <tshi206@aucklanduni.ac.nz>
2018-07-06 16:53:53 +1200

    add api controller, port 8080 must be explicitly opened and listened by hosting service in order to use the api

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -4,6 +4,9 @@ namespace OCA\OwnNotes\AppInfo;
 
 $application = new Application();
 $application->registerRoutes($this, [
+	'resources' => [
+		'note_api' => ['url' => '/api/0.1/notes']
+	],
     'routes' => [
         [
             // The handler is the PageController's index method
@@ -17,6 +20,8 @@ $application->registerRoutes($this, [
 		['name' => 'note#show', 'url' => '/notes/{id}', 'verb' => 'GET'],
 		['name' => 'note#create', 'url' => '/notes', 'verb' => 'POST'],
 		['name' => 'note#update', 'url' => '/notes/{id}', 'verb' => 'PUT'],
-		['name' => 'note#destroy', 'url' => '/notes/{id}', 'verb' => 'DELETE']
+		['name' => 'note#destroy', 'url' => '/notes/{id}', 'verb' => 'DELETE'],
+		['name' => 'note_api#preflighted_cors', 'url' => '/api/0.1/{path}',
+			'verb' => 'OPTIONS', 'requirements' => ['path' => '.+']]
     ]
 ]);
